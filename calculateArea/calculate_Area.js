110d58bb987c755f138a5794c4d37ef058ee439f
moisesefsantos <moises.efsantos@gmail.com>
2025-04-15 18:01:21 -0300

    add grocery tracker

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,15 @@ function calculateArea() {
     document.getElementById('result').innerHTML = `The area of the rectangle is: ${area}`;
 }
 
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateTotalAmount() {
+    grocery1 = parseFloat(document.getElementById("grocery1").value);
+    grocery2 = parseFloat(document.getElementById("grocery2").value);
+    grocery3 = parseFloat(document.getElementById("grocery3").value);
+
+    let totalAmount = grocery1 + grocery2 + grocery3;
+    document.getElementById('totalAmount').innerHTML = `The total amount of groceries is: $ ${totalAmount}`;
+}
